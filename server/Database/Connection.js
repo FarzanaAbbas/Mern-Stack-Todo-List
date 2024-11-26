4ea3b3c8001dfa8038b338af94833c8dfaa966bd
FarzanaAbbas <farzu502@gmail.com>
2024-11-26 09:09:26 +0530

    updated

--- server/Database/Connection.js
+++ server/Database/Connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 function RunServer(){
     try {
-        mongoose.connect('mongodb://localhost:27017/todolist');
+        mongoose.connect(process.env.MONGO_URI);
         console.log('mongoDB is connected!')
     } catch (error) {
         console.log(error.message)
